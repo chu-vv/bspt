f336d530cb26279e66cc444afb8d8c65f8e2d786
chu-vv <chu.vv@mail.ru>
2017-02-06 18:17:54 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #runs sflow-analysis.py as "sflow-analysis" locally using 4 threads to analyse data from sflow-0118.csv file 
 
 #via spark-submit
-spark-submit sflow-analysis.py local[4] "sflow-analysis" "sflow-0118.csv" >  out_tech.txt
+spark-submit sflow-analysis.py local[4] "sflow-analysis" "one.csv" >  out_tech.txt
 
 #or via python  
-sflow-analysis.py local[4] "sflow-analysis" "sflow-0118.csv" >  out_tech.txt
+sflow-analysis.py local[4] "sflow-analysis" "one.csv" >  out_tech.txt
